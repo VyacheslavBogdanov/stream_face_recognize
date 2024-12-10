1f8719919ab9f88f8e020e467c5c47e0ac9a91d1
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2024-12-10 14:25:59 +0400

    develop fix

--- src/components/FireDetection/FireDetection.vue
+++ src/components/FireDetection/FireDetection.vue
@@ -30,60 +30,63 @@ const updateImage = (base64: string) => {
 	imageBase64.value = base64;
 };
 
-
 const sendRequest = async () => {
-  if (!imageBase64.value) {
-    console.error('Изображение не выбрано');
-    return;
-  }
+	if (!imageBase64.value) {
+		console.error('Изображение не выбрано');
+		return;
+	}
 
-  const base64Image = imageBase64.value.replace(/^data:image\/[a-z]+;base64,/, '');
+	const base64Image = imageBase64.value.replace(/^data:image\/[a-z]+;base64,/, '');
 
-  const requestId = uuidv4();
+	const requestId = uuidv4();
 
-  console.log('requestId:', requestId);
-  console.log('base64Image:', base64Image);
+	console.log('requestId:', requestId);
+	console.log('base64Image:', base64Image);
 
-  try {
-    const response = await fetch('/api/predict', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        thresholds: [
-          {
-            move_confidence: 0.2,
-            move_velocity: 0.3,
-            static_confidence: 0.7,
-            type: 'person',
-          },
-        ],
-        sabotage_threshold: 22,
-        requestId: requestId,
-        image: base64Image,
-      }),
-    });
+	try {
+		const response = await fetch('/api/predict', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				thresholds: [
+					{
+						move_confidence: 0.2,
+						move_velocity: 0.3,
+						static_confidence: 0.7,
+						type: 'person',
+					},
+					{
+						move_confidence: 0.2,
+						move_velocity: 0.3,
+						static_confidence: 0.7,
+						type: 'vehicle',
+					},
+				],
+				sabotage_threshold: 22,
+				requestId: requestId,
+				image: base64Image,
+			}),
+		});
 
-    if (!response.ok) {
-      throw new Error(`Ошибка: ${response.status} ${response.statusText}`);
-    }
+		if (!response.ok) {
+			throw new Error(`Ошибка: ${response.status} ${response.statusText}`);
+		}
 
-    const data = await response.json();
-    console.log('Ответ от сервера:', data);
+		const data = await response.json();
+		console.log('Ответ от сервера:', data);
 
-    if (data.objects && data.objects.length > 0) {
-      result.value = { type: data.objects[0].type };
-    } else {
-      result.value = { type: 'no_fire' };
-    }
-  } catch (error) {
-    console.error('Ошибка при запросе:', error);
-    result.value = { type: 'no_fire' };
-  }
-
+		if (data.objects && data.objects.length > 0) {
+			result.value = { type: data.objects[0].type };
+		} else {
+			result.value = { type: 'no_fire' };
+		}
+	} catch (error) {
+		console.error('Ошибка при запросе:', error);
+		result.value = { type: 'no_fire' };
+	}
 };
-
 </script>
 
 <style lang="scss" scoped>
