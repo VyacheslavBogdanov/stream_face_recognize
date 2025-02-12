1634f5b0fd26d139bcf734cc99bef68701a856c0
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2025-02-12 15:18:45 +0400

    stream face recognize in process

---
+++ src/router/router.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router'; // Импорт типа
+
+// import Home from '../views/Home.vue';
+// import Projects from '../views/Projects.vue';
+// import Contacts from '../views/Contacts.vue';
+
+// Определяем маршруты с типизацией
+// const routes: RouteRecordRaw[] = [
+// 	{ path: '/', component: Home },
+// 	{ path: '/projects', component: Projects },
+// 	{ path: '/contacts', component: Contacts },
+// ];
+
+// Создаем экземпляр роутера
+// const router = createRouter({
+// 	history: createWebHistory(),
+// 	routes, // Передаем маршруты
+// });
+
+// export default router;
