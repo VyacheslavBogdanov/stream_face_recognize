7174691663ff493d47e0b7b09b75f5021c08dddb
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2025-02-12 15:50:41 +0400

    recording the changes

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import type { RouteRecordRaw } from 'vue-router'; // Импорт типа
+import type { RouteRecordRaw } from 'vue-router';
 
-// import Home from '../views/Home.vue';
-// import Projects from '../views/Projects.vue';
-// import Contacts from '../views/Contacts.vue';
+import SearchByPhoto from '@/views/SearchByPhoto/SearchByPhoto.vue';
+import ComparisonOfTwoPhotos from '@/views/ComparisonOfTwoPhotos/ComparisonOfTwoPhotos.vue';
+import DataBase from '@/views/DataBase/DataBase.vue';
 
-// Определяем маршруты с типизацией
-// const routes: RouteRecordRaw[] = [
-// 	{ path: '/', component: Home },
-// 	{ path: '/projects', component: Projects },
-// 	{ path: '/contacts', component: Contacts },
-// ];
+const routes: RouteRecordRaw[] = [
+	{ path: '/', component: SearchByPhoto },
+	{ path: '/comparison-of-two-photos', component: ComparisonOfTwoPhotos },
+	{ path: '/data-base', component: DataBase },
+];
 
-// Создаем экземпляр роутера
-// const router = createRouter({
-// 	history: createWebHistory(),
-// 	routes, // Передаем маршруты
-// });
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+});
 
-// export default router;
+export default router;
