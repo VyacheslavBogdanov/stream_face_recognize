781493b64788edf6b7ab158feb63f08347bf5ba6
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2025-03-05 15:40:21 +0400

    fix sync db

--- src/views/DataBase/DataBase.vue
+++ src/views/DataBase/DataBase.vue
@@ -57,19 +57,19 @@ const addFace = async () => {
 	if (!newFace.value.name || !newFace.value.photoUrl) return;
 	const id = uuidv4();
 	try {
-		// const base64Image = await urlToBase64(newFace.value.photoUrl);
-		// const imageBase64 = base64Image.replace(/^data:image\/[a-z]+;base64,/, '');
+		const base64Image = await urlToBase64(newFace.value.photoUrl);
+		const imageBase64 = base64Image.replace(/^data:image\/[a-z]+;base64,/, '');
 
-		// const response = await fetch(`${HOST}/add_new_face`, {
-		// 	method: 'POST',
-		// 	headers: { 'Content-Type': 'application/json' },
-		// 	body: JSON.stringify({
-		// 		request_id: uuidv4(),
-		// 		id: id,
-		// 		image: imageBase64,
-		// 	}),
-		// });
-		// if (!response.ok) throw new Error('Ошибка добавления вектора');
+		const response = await fetch(`${HOST}/add_new_face`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				request_id: uuidv4(),
+				id: id,
+				image: imageBase64,
+			}),
+		});
+		if (!response.ok) throw new Error('Ошибка добавления вектора');
 
 		const db = await fetch(DB, {
 			method: 'POST',
@@ -134,27 +134,52 @@ const clearDatabase = async () => {
 
 const syncDB = async () => {
 	if (vectors.value.length === faces.value.length) return;
-	try {
-		await Promise.all(
-			faces.value.map(async (face) => {
-				const base64Image = await urlToBase64(face.photoUrl);
-				const imageBase64 = base64Image.replace(/^data:image\/[a-z]+;base64,/, '');
+	const faceIds = faces.value.map((face) => face.id);
+	const vectorIds = new Set(vectors.value);
 
-				return fetch(`${HOST}/add_new_face`, {
-					method: 'POST',
-					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify({
-						request_id: uuidv4(),
-						id: face.id,
-						image: imageBase64,
+	if (vectors.value.length < faces.value.length) {
+		try {
+			await Promise.all(
+				faces.value
+					.filter((face) => !vectorIds.has(face.id))
+					.map(async (face) => {
+						const base64Image = await urlToBase64(face.photoUrl);
+						const imageBase64 = base64Image.replace(/^data:image\/[a-z]+;base64,/, '');
+
+						return fetch(`${HOST}/add_new_face`, {
+							method: 'POST',
+							headers: { 'Content-Type': 'application/json' },
+							body: JSON.stringify({
+								request_id: uuidv4(),
+								id: face.id,
+								image: imageBase64,
+							}),
+						});
 					}),
-				});
-			}),
-		);
-		fetchFaces();
-	} catch (error) {
-		console.error(error);
+			);
+		} catch (error) {
+			console.error(error);
+		}
+	} else {
+		const extraVectors = vectors.value.filter((id) => !faceIds.includes(id));
+		try {
+			await Promise.all(
+				extraVectors.map(async (id) => {
+					return fetch(`${HOST}/delete_face`, {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({
+							request_id: uuidv4(),
+							items: [id],
+						}),
+					});
+				}),
+			);
+		} catch (error) {
+			console.error(error);
+		}
 	}
+	fetchFaces();
 };
 
 onMounted(fetchFaces);
