111ad7482582568c11ae10d7ad16bac38c2b6f1a
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2025-03-27 16:34:35 +0400

    fix db

--- src/views/DataBase/DBForm/DBForm.vue
+++ src/views/DataBase/DBForm/DBForm.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import type { FaceDB } from '../../../components/utils/types.ts';
+
 const props = defineProps<{ newFace: FaceDB }>();
 const emit = defineEmits(['update:newFace', 'addFace']);
+
+const fileName = ref<string>('Загрузите файл...');
 
 const onFileChange = (event: Event) => {
 	const target = event.target as HTMLInputElement;
 	if (!target.files?.[0]) return;
+
+	const file = target.files[0];
+	fileName.value = file.name;
 
 	const reader = new FileReader();
 	reader.onload = () => {
@@ -15,7 +21,7 @@ const onFileChange = (event: Event) => {
 			photoUrl: reader.result as string,
 		});
 	};
-	reader.readAsDataURL(target.files[0]);
+	reader.readAsDataURL(file);
 };
 </script>
 
@@ -35,15 +41,16 @@ const onFileChange = (event: Event) => {
 					})
 			"
 		/>
-
-		<input
-			type="file"
-			accept="image/*"
-			placeholder="Введите URL изображения"
-			class="form__input"
-			required
-			@change="onFileChange"
-		/>
+		<div class="form__file-input">
+			<label for="fileUpload" class="form__file-label">{{ fileName }}</label>
+			<input
+				id="fileUpload"
+				type="file"
+				accept="image/*"
+				class="form__file-hidden"
+				@change="onFileChange"
+			/>
+		</div>
 		<button type="submit" class="form__button">Добавить в БД</button>
 	</form>
 </template>
@@ -56,7 +63,6 @@ const onFileChange = (event: Event) => {
 	gap: 10px;
 	margin-bottom: 20px;
 
-	th,
 	&__input {
 		flex: 1;
 		padding: 8px;
@@ -64,6 +70,32 @@ const onFileChange = (event: Event) => {
 		box-shadow:
 			0px 3px 8px -6px rgba(24, 39, 75, 0.05),
 			0px 10px 36px -4px rgba(17, 24, 41, 0.1);
+	}
+
+	&__file-input {
+		position: relative;
+		flex: 1;
+		border: 1px solid #ccc;
+		padding: 8px;
+		text-align: center;
+		background-color: white;
+		box-shadow:
+			0px 3px 8px -6px rgba(24, 39, 75, 0.05),
+			0px 10px 36px -4px rgba(17, 24, 41, 0.1);
+	}
+
+	&__file-hidden {
+		position: absolute;
+		opacity: 0;
+		width: 100%;
+		height: 100%;
+	}
+
+	&__file-label {
+		display: flex;
+		width: 100%;
+		font-size: smaller;
+		opacity: 0.75;
 	}
 
 	&__button {
